9d863b154fe1cfb56c2a84ef2dc7b5bb493145ea
Evgeniya Yurieva <evgeniya.yurieva@gmail.com>
2020-06-02 20:58:01 +1000

    add mongodb

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const mongoos = require('mongoose');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
+require('./models/user');
+require('./services/passport');
+
+mongoos.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log(req.query.f);
-    res.send(process.env);
-    console.log(process.env);
-});
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log('Node.js has started on port ' + PORT));
